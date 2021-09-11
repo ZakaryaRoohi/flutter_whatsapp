9af9406878fd9c422e26817601870615245f25b4
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-09-11 16:29:38 +0430

    add TabBar elements

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -5,13 +5,34 @@ class WhatsAppHome extends StatefulWidget {
   State<StatefulWidget> createState() => WhatsAppHomeState();
 }
 
-class WhatsAppHomeState extends State<WhatsAppHome> {
+class WhatsAppHomeState extends State<WhatsAppHome>  with SingleTickerProviderStateMixin{
+
+  late TabController tabController ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = new TabController(length: 4, vsync: this);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
       title: new Text('واتساپ'),
       elevation: 5,
+      bottom: new TabBar(
+        controller: tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab( text: "چت ها",),
+            new Tab( text: "وضعیت",),
+            new Tab( text: "تماس ها",),
+          ]
+      ),
       actions: [
         new Icon(Icons.search),
         new Padding(padding: const EdgeInsets.symmetric(horizontal: 8)),
