52e6704896138cd9ee1b97a1fde15b22f2cc5761
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-09-11 16:44:32 +0430

    handle tab bar changing

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp/pages/call_screen.dart';
+import 'package:flutter_whatsapp/pages/camera_screen.dart';
+import 'package:flutter_whatsapp/pages/chat_screen.dart';
+import 'package:flutter_whatsapp/pages/status_screen.dart';
 
 class WhatsAppHome extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => WhatsAppHomeState();
 }
 
-class WhatsAppHomeState extends State<WhatsAppHome>  with SingleTickerProviderStateMixin{
-
-  late TabController tabController ;
+class WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
 
   @override
   void initState() {
@@ -16,28 +20,41 @@ class WhatsAppHomeState extends State<WhatsAppHome>  with SingleTickerProviderSt
     tabController = new TabController(length: 4, vsync: this);
   }
 
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-      title: new Text('واتساپ'),
-      elevation: 5,
-      bottom: new TabBar(
-        controller: tabController,
-          indicatorColor: Colors.white,
-          tabs: <Widget>[
-            new Tab(icon: new Icon(Icons.camera_alt)),
-            new Tab( text: "چت ها",),
-            new Tab( text: "وضعیت",),
-            new Tab( text: "تماس ها",),
-          ]
+      appBar: new AppBar(
+        title: new Text('واتساپ'),
+        elevation: 5,
+        bottom: new TabBar(
+            controller: tabController,
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              new Tab(icon: new Icon(Icons.camera_alt)),
+              new Tab(
+                text: "چت ها",
+              ),
+              new Tab(
+                text: "وضعیت",
+              ),
+              new Tab(
+                text: "تماس ها",
+              ),
+            ]),
+        actions: [
+          new Icon(Icons.search),
+          new Padding(padding: const EdgeInsets.symmetric(horizontal: 8)),
+          new Icon(Icons.more_vert)
+        ],
       ),
-      actions: [
-        new Icon(Icons.search),
-        new Padding(padding: const EdgeInsets.symmetric(horizontal: 8)),
-        new Icon(Icons.more_vert)
-      ],
-    ));
+      body: new TabBarView(
+          controller: tabController,
+          children: <Widget>[
+        new CameraScreen(),
+        new ChatScreen(),
+        new StatusScreen(),
+        new CallScreen()
+      ]),
+    );
   }
 }
