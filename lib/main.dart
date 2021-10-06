e87c874482e1a5f49d1a3d339beb62178e5a8878
ZakaryaRoohi <zakarya.roohi@gmail.com>
2021-10-06 17:02:29 +0330

    demo animation

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,30 @@ class AnimationDemo extends StatefulWidget {
 
 class AnimationDemoState extends State<AnimationDemo>
     with SingleTickerProviderStateMixin {
-
   late AnimationController _controller;
-
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     //موقه ای که این صفحه بسته بشه باید این کنترلر هم کیل بشه
-    _controller = AnimationController(vsync: this,duration: Duration(milliseconds: 2000));
-
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
   }
+
   @override
   void dispose() {
-    // TODO: implement dispose
     _controller.dispose();
     super.dispose();
+  }
+
+  _playAnimation() async {
+    try{
+      await _controller.forward();
+      await _controller.reverse();
+    }on TickerCanceled{
+
+    }
+
   }
 
   @override
@@ -44,7 +51,9 @@ class AnimationDemoState extends State<AnimationDemo>
         title: Text('Stagger Animation'),
       ),
       body: GestureDetector(
-        onTap: () {},
+        onTap: () {
+          _playAnimation();
+        },
         child: Center(
           child: Container(
             width: 300,
@@ -52,9 +61,7 @@ class AnimationDemoState extends State<AnimationDemo>
             decoration: BoxDecoration(
                 color: Colors.black.withOpacity(0.1),
                 border: Border.all(color: Colors.black.withOpacity(0.5))),
-            child: StaggerAnimation(
-              controller : _controller.view
-            ),
+            child: StaggerAnimation(controller: _controller.view),
           ),
         ),
       ),
@@ -62,27 +69,65 @@ class AnimationDemoState extends State<AnimationDemo>
   }
 }
 
+class StaggerAnimation extends StatelessWidget {
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius?> borderRadius;
+  final Animation<Color?> color;
 
-class StaggerAnimation extends StatelessWidget{
-  var controller;
+  StaggerAnimation({required this.controller})
+      : opacity = Tween<double>(begin: 0.0, end: 1.0).animate(
+            new CurvedAnimation(
+                parent: controller,
+                curve: Interval(0.0, 0.100, curve: Curves.ease))),
+        width = Tween<double>(begin: 50.0, end: 150.0).animate(CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.125, 0.250, curve: Curves.ease))),
+        height = Tween<double>(begin: 50.0, end: 150.0).animate(CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.250, 0.375, curve: Curves.ease))),
+        padding = EdgeInsetsTween(
+                begin: const EdgeInsets.only(bottom: 16),
+                end: const EdgeInsets.only(bottom: 75))
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: Interval(0.250, 0.375, curve: Curves.ease))),
+        borderRadius = BorderRadiusTween(
+                begin: BorderRadius.circular(4), end: BorderRadius.circular(75))
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: Interval(0.375, 0.500, curve: Curves.ease))),
+        color = ColorTween(begin: Colors.indigo[100], end: Colors.orange[400])
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: Interval(0.500, 0.750, curve: Curves.ease)));
 
-
-  StaggerAnimation({this.controller});
-
-  // final Animation<double>
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return AnimatedBuilder(animation: controller, builder: buildAnimation);
   }
 
+  Widget buildAnimation(BuildContext context, Widget? child) {
+    return Container(
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+              color: color.value,
+              border: Border.all(color: Colors.indigo, width: 3),
+              borderRadius: borderRadius.value),
+        ),
+      ),
+    );
+  }
 }
-
-
-
-
-
-
 
 //مربوط به اولین انیمیشن
 // class MyApp extends StatefulWidget {
